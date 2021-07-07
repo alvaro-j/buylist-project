4e6642cffcc5985292d3127ee778edafe1f93d6b
alvaro-j <86482525+alvaro-j@users.noreply.github.com>
2021-07-07 18:13:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ let remover = document.getElementById("remover")
 add.addEventListener('click', addOptionSelect)
 remover.addEventListener('click', removeOptionSelect)
 let inputtxt = document.getElementById('input')
+let inputnumber = document.getElementById('inputqtd')
 
 function addOptionSelect() {
   let select = document.getElementById("lista");
   let option = document.createElement("option");
-  option.text = input.value;
+  option.text = input.value + "  Qtd.:" + inputqtd.value;
   select.add(option);
   inputtxt.value = ''
+  inputnumber.value= ''
 }
 
 function removeOptionSelect() {
